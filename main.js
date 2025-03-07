dc631ab0158e669ee8daaaaf5b877e097086f9de
Jhon Dave Layva <127804493+JhonDaveLayva@users.noreply.github.com>
2025-03-07 19:15:52 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const subjectSelector = document.getElementById("subjectContainer")
+let jsonFile = []
+fetch("main contents.json").then(res => {
+    return res.json()
+}).then(load => {
+    jsonFile = [...load]
+    main()
+})
+
+main = () => {
+    subjectSelector.innerHTML = jsonFile.map(element => {
+        let subjectImage;
+        if (element["image-path"] == "") {
+            subjectImage = "images/image-not-found.jpg"
+        } else {
+            subjectImage = element["image-path"]
+        }
+
+        return `<div class="subject-div">
+                    <div class="subject-elements">
+                        <div class="subject-img">
+                            <img src= ${subjectImage} alt="ecen 102 picture">
+                        </div>
+                        <div class="subject-description">
+                            <h1 class="subject-name">${element["course-name"]}</h1>
+                            <div class="other-details">
+                                <p class="course-code">Course Code: ${element["course-code"]}</p>
+                                <p class="lesson-count">Lessons: ${element["lessons"].length}</p>
+                            </div>
+                            <h1 class="start-btn" data-index = ${element["course-index"]}>Review this Lesson</h1>
+                        </div>
+                     </div>
+                </div>`
+    }).join("")
+
+    const subjects = Array.from(document.getElementsByClassName("start-btn"))
+    subjects.forEach(subject => {
+        subject.addEventListener("click", e => {
+            const selectedSubject = e.target
+            localStorage.setItem("subjectChosen", selectedSubject.dataset["index"])                
+            return window.location.assign("/lesson.html")
+        })
+    })
+}
